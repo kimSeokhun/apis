e272d4583a8d9006eb7807b063ec4c2fc7ac5ffc
Brant Hwang <dlstj3039@gmail.com>
2017-01-03 16:01:19 +0900

    file up download 추가

--- src/main/java/com/chequer/ax5/api/demo/entity/file/AX5File.java
+++ src/main/java/com/chequer/ax5/api/demo/entity/file/AX5File.java
@@ -5,11 +5,12 @@ import lombok.Data;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
-public class AX5File {
+public class AX5File implements Comparable {
 
     private String id;
 
@@ -22,7 +23,12 @@ public class AX5File {
     private String createdAt;
 
     @JsonIgnore
+    private File file;
+
+    @JsonIgnore
     private MultipartFile multipartFile;
+
+    private long lastModified;
 
     public static AX5File of(MultipartFile multipartFile) {
         AX5File ax5File = new AX5File();
@@ -43,5 +49,19 @@ public class AX5File {
 
     public String getJsonName() {
         return String.format("%s.json", id, ext);
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        AX5File ax5File = (AX5File) o;
+
+        long result = lastModified - ax5File.lastModified;
+        if (result < 0) {
+            return -1;
+        } else if (result > 0) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
