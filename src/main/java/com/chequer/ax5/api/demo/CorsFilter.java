9211e7d843ce34ee8b74316f758e026bce203693
Brant Hwang <dlstj3039@gmail.com>
2017-01-03 14:15:49 +0900

    CorsFilter 추가

--- src/main/java/com/chequer/ax5/api/demo/CorsFilter.java
+++ src/main/java/com/chequer/ax5/api/demo/CorsFilter.java
@@ -16,7 +16,7 @@ public class CorsFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
