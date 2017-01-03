e256163a5e88f98fb6422e2b346b0d9f25d52bfb
Thomas Jang <tom@axisj.com>
2017-01-03 16:05:15 +0900

    프로퍼티~

--- src/main/java/com/chequer/ax5/api/demo/CorsFilter.java
+++ src/main/java/com/chequer/ax5/api/demo/CorsFilter.java
@@ -1,5 +1,6 @@
 package com.chequer.ax5.api.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
@@ -9,6 +10,9 @@ import java.io.IOException;
 @Component
 public class CorsFilter implements Filter {
 
+    @Value("${allow.origin}")
+    private String allowOrigin;
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
 
@@ -17,7 +21,7 @@ public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", allowOrigin);
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with, x-auth-token, Content-Type, Origin, Accept");
